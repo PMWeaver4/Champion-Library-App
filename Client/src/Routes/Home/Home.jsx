9ff16b3250e6996b52b4014e975153fec66aa8ec
Genessi Barot <genessibarot@gmail.com>
2024-03-27 17:08:49 -0400

    home page (dashboard) & navbar CSS complete (also made some edits to scss syntax in the files

--- Client/src/Routes/Home/Home.jsx
+++ Client/src/Routes/Home/Home.jsx
@@ -1,3 +1,13 @@
+import Navbar from "../../Components/Navbar/Navbar";
 export default function Home() {
-  return <main className="home-page"></main>;
+  return (
+    // home page that contains the "dashboard or main page once logged in"
+    <main className="home-page">
+      <Navbar />
+      <div className="home-banner">
+        <h1 className="welcome-msg">Welcome to South Meadow's Lending Library!</h1>
+        <h2 className="slogan">Building Community Beyond Books</h2>
+      </div>
+    </main>
+  );
 }
