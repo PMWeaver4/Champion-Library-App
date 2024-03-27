961d3c74d404e073a58c74b486200857bac0c068
ttnclinton <ttnclinton@gmail.com>
2024-03-27 01:57:46 -0600

    setup backbones files to server

---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+const userRoutes = require("./user_routes");
+
+const roomRoutes = require("./room_routes");
+
+const messageRoutes = require("./message_routes");
+
+module.exports = { userRoutes, roomRoutes, messageRoutes};
