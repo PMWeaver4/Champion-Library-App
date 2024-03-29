6b6190d6902429ad9e833336b6abb0eabc5a134f
Genessi Barot <genessibarot@gmail.com>
2024-03-29 12:45:06 -0400

    inbox css (mobile, tablet,desktop) css is done but need to revise to fix the cutting off of content with overflow scroll

--- Client/src/Routes/Inbox/Inbox.jsx
+++ Client/src/Routes/Inbox/Inbox.jsx
@@ -1,17 +1,39 @@
 import Navbar from "../../Components/Navbar/Navbar";
+import NotificationTile from "./NotificationTile";
 
 export default function Inbox() {
-  return ( 
-  <main className="inbox-page">
-    <Navbar pageTitle="Inbox"/>
-    <div className="inbox-content-container">
-      <div className="notification-panel">
-        <input className="inbox-search" type="text" placeholder="Search"></input>
+  return (
+    <main className="inbox-page">
+      <Navbar pageTitle="Inbox" />
+      <div className="inbox-content-container">
+        <div className="notification-panel">
+          <div className="input-container">
+            <input className="inbox-search" type="text" placeholder="Search"></input>
+            <button className="draft-msg-btn">
+              <i className="fa-solid fa-circle-plus"></i>
+            </button>
+          </div>
+          <div className="notification-tile-container">
+            {/* placeholders */}
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+            <NotificationTile />
+          </div>
+        </div>
+        <div className="inbox-panel">
+          <button className="panel-back-btn">
+            <i className="fa-solid fa-arrow-left"></i>
+          </button>
+        </div>
       </div>
-      <div className="inbox-panel">
-        <button className="panel-back-btn"><i className="fa-solid fa-arrow-left"></i></button>
-      </div>
-    </div>
-  </main>
+    </main>
   );
 }
