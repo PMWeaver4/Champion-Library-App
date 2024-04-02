abcdcd4523c55e277e59dbdcfb11a644acfced0d
ttnclinton <ttnclinton@gmail.com>
2024-04-02 00:21:09 -0600

    added dependencies to outside layer of packagejson which might need some fixing to the file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const userRoute = require("./user_route");
 
 const bookRoutes = require("./book_routes");
@@ -6,16 +6,3 @@ const bookRoutes = require("./book_routes");
 module.exports = { userRoute, bookRoutes};
 
 
- 
-
-=======
-const userRoutes = require("./user_route");
-
-const bookRoutes = require("./book_routes");
-
-
-module.exports = { userRoutes, bookRoutes};
-
-
-
->>>>>>> a672deb5f82b6a07aa958121c728c99210a6f3eb
