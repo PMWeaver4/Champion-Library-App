1fbfa9a766e96b9dadea8a9304fe1d9d181e31a7
Genessi Barot <genessibarot@gmail.com>
2024-04-02 17:07:51 -0400

    functioning navbar with corresponding page titles

--- Client/src/Routes/MyLibrary/MyLibrary.jsx
+++ Client/src/Routes/MyLibrary/MyLibrary.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import MenuPopup from "../../Components/MenuPopup/MenuPopup";
+import Navbar from "../../Components/Navbar/Navbar";
+
 export default function MyLibrary() {
-  return <main className="my-library-page"></main>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <main className="my-library-page">
+      <Navbar toggleMenu={toggleMenu} pageTitle="My Library" />
+      {isMenuOpen && <MenuPopup />}
+    </main>
+  );
 }
