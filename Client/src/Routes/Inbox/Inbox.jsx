cbdb2e5a76a6b6d4d04a7df8623a65e1933b22ad
Genessi Barot <genessibarot@gmail.com>
2024-04-03 15:45:39 -0400

    card changes

--- Client/src/Routes/Inbox/Inbox.jsx
+++ Client/src/Routes/Inbox/Inbox.jsx
@@ -1,48 +1,13 @@
 import { useState } from "react";
 import MenuPopup from "../../Components/MenuPopup/MenuPopup";
 import Navbar from "../../Components/Navbar/Navbar";
-import NotificationTile from "./NotificationTile";
+// import NotificationTile from "./NotificationTile";
 
 export default function Inbox() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  function toggleMenu() {
-    setIsMenuOpen(!isMenuOpen);
-  }
-
   return (
     <main className="inbox-page">
       <Navbar toggleMenu={toggleMenu} pageTitle="Inbox" />
       {isMenuOpen && <MenuPopup />}
-      <div className="inbox-content-container">
-        <div className="notification-panel">
-          <div className="input-container">
-            <input className="inbox-search" type="text" placeholder="Search"></input>
-            <button className="draft-msg-btn">
-              <i className="fa-solid fa-circle-plus"></i>
-            </button>
-          </div>
-          <div className="notification-tile-container">
-            {/* placeholders */}
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-            <NotificationTile />
-          </div>
-        </div>
-        <div className="inbox-panel">
-          <button className="panel-back-btn">
-            <i className="fa-solid fa-arrow-left"></i>
-          </button>
-        </div>
-      </div>
     </main>
   );
 }
