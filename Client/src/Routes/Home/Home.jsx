f5881817336285d061527175d650ccbf1fda27e9
Genessi Barot <genessibarot@gmail.com>
2024-04-04 19:47:38 -0400

    new homepage  css  (mobile,  tablet, desktop)

--- Client/src/Routes/Home/Home.jsx
+++ Client/src/Routes/Home/Home.jsx
@@ -1,8 +1,11 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@shadcn/components/ui/carousel";
+
 // import BookProfileCard from "../../Components/BookProfileCard/BookProfileCard";
 
 import { useState } from "react";
 import MenuPopup from "../../Components/MenuPopup/MenuPopup";
-import Navbar from "../../Components/Navbar/Navbar";
+import NavigationBar from "../../Components/Navigation/NavigationBar";
+import BookTile from "../../Components/BookProfileCard/BookTile";
 export default function Home() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -12,31 +15,108 @@ export default function Home() {
   return (
     // home page that contains the "dashboard or main page once logged in"
     <main className="home-page">
-      <Navbar toggleMenu={toggleMenu} pageTitle="Dashboard" />
+      <NavigationBar toggleMenu={toggleMenu} pageTitle="HOME" />
       {isMenuOpen && <MenuPopup />}
       {/* <BookProfileCard /> */}
       {/* will need to make book profile card open when book tile is clicked same format will be done for item tile */}
-      <div className="home-banner">
-        <h1 className="welcome-msg">Welcome to South Meadow's Lending Library!</h1>
-        <h2 className="slogan">Building Community Beyond Books</h2>
-      </div>
-      <div className="dashboard-display-container">
-        <div className="display-shelf-headers">
-          <h3>Books</h3>
-          <button className="view-books-btn">View All</button>
+        <div className="home-body">
+          <div className="home-banner">
+            <h1 className="welcome-msg">Welcome to South Meadow's Lending Library!</h1>
+            <h2 className="slogan">Building Community Beyond Books</h2>
+          </div>
+        <div className="home-section">
+          <div className="books-container">
+            <div className="view-container">
+              <h3>BOOKS</h3>
+              <button className="view-btn view-books-btn">View all</button>
+            </div>
+            <Carousel className="w-8/12 self-center">
+              <CarouselContent>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+          <div className="board-games-container">
+            <div className="view-container">
+              <h3>GAMES</h3>
+              <button className="view-btn view-boardgames-btn">View all</button>
+            </div>
+            <Carousel className="w-8/12 self-center">
+              <CarouselContent>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+          <div className="others-container">
+            <div className="view-container">
+              <h3>OTHER</h3>
+              <button className="view-btn view-others-btn">View all</button>
+            </div>
+            <Carousel className="w-8/12 self-center">
+              <CarouselContent>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+                <CarouselItem className="basis-1/3 md:basis-1/4 lg:basis-1/5">
+                  <BookTile />
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+          </div>
         </div>
-        <div className="display-shelf books"></div>
-        <div className="display-shelf-headers">
-          <h3>Board Games</h3>
-          <button className="view-games-btn">View All</button>
-        </div>
-        <div className="display-shelf boardgames"></div>
-        <div className="display-shelf-headers">
-          <h3>Miscellaneous Items</h3>
-          <button className="view-items-btn">View All</button>
-        </div>
-        <div className="display-shelf items"></div>
-      </div>
     </main>
   );
 }
