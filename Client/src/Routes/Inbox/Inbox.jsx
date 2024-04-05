91547496da0c69d690c722e6a0bf1f60a5c354d9
Genessi Barot <genessibarot@gmail.com>
2024-04-05 16:43:14 -0400

    new inbox page css

--- Client/src/Routes/Inbox/Inbox.jsx
+++ Client/src/Routes/Inbox/Inbox.jsx
@@ -5,16 +5,39 @@ import NavigationBar from "../../Components/Navigation/NavigationBar";
 // import NotificationTile from "./NotificationTile";
 
 export default function Inbox({ toggleMenu, pageTitle }) {
+  // opening - closing menu (mobile)
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   function toggleMenu() {
     setIsMenuOpen(!isMenuOpen);
   }
+  // for tab system, starts on all tab
+  const [activeTab, setActiveTab] = useState("allRequest");
+// Req = request
+  const tabs = [
+    { id: "allRequest", title: "All", content: "Content for Tab 1" },
+    { id: "borrowReq", title: "Borrow Request", content: "Content for Tab 2" },
+    { id: "returnReq", title: "Return Request", content: "Content for Tab 3" },
+  ];
+
   return (
     <main className="inbox-page">
       <NavigationBar toggleMenu={toggleMenu} pageTitle="INBOX" />
       {isMenuOpen && <MenuPopup />}
-      <div></div>
+      {/* <div className="inbox"> */}
+      <div className="inbox-body">
+        <div className="inbox-container">
+          <div className="inbox-tabs">
+            {tabs.map((tab) => (
+              <button key={tab.id} className={`tab ${activeTab === tab.id ? "active" : ""}`} onClick={() => setActiveTab(tab.id)}>
+                {tab.title}
+              </button>
+            ))}
+          </div>
+          <div className="inbox-tab-content">{tabs.find((tab) => tab.id === activeTab)?.content}</div>
+        </div>
+      </div>
+      {/* </div> */}
     </main>
   );
 }
