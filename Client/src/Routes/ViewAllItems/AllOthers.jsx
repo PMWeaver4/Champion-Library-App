2833bc68cc2fa69bd2e89604bb3bc0de0db52169
Genessi Barot <genessibarot@gmail.com>
2024-04-06 16:25:22 -0400

    homePage buttons have functionality (can open view all pages and come back to home page)

--- Client/src/Routes/ViewAllItems/AllOthers.jsx
+++ Client/src/Routes/ViewAllItems/AllOthers.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import PageTemplate from "../../Components/PageTemplate/PageTemplate";
 
 export default function AllOthers({pageTitle}) {
@@ -6,7 +7,7 @@ export default function AllOthers({pageTitle}) {
       <PageTemplate pageTitle="OTHER">
         <div className="all-others-body">
           <div className="view-all-headers">
-          <button><i className="fa-solid fa-arrow-left"></i></button>
+          <NavLink to="/home"><i className="fa-solid fa-arrow-left"></i></NavLink>
             <h1>OTHER</h1>
           </div>
           <div className="view-all-grid"></div>
