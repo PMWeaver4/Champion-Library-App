8a6b1799ab30312c91a2c8bf27eef21836e7d824
PMWeaver4 <paulmarrottweaver@gmail.com>
2024-05-08 05:15:01 -0400

    apparently I made a change to inbox

--- Client/src/Components/ItemProfileCard/GamesProfileCard.jsx
+++ Client/src/Components/ItemProfileCard/GamesProfileCard.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { getToken } from "../../localStorage";
+import config from "../../config.json";
 
 export default function GamesProfileCard({ game }) {
   // maximum characters
   const MAX_CHAR = 30;
 
   const navigate = useNavigate();
+  
+  const [owner, setOwner] = useState("");
+  const [item, setItem] = useState("");
+
+function borrowGame() {
+ //do the create notification, config notifications/create, with a key of book: book._id or item: item._id
+
+setOwner(game.user)
+setItem(game)
+ // Construct book data from state
+ const requestData = {
+   item,
+   owner
+ };
+ if(game.hasPendingRequest){
+   alert ("this game already has a pending request");
+   throw new Error ("This game already has a pending request")
+ }
+ // Fetch configuration
+ fetch(config.backend_url + `notifications/create/`, {  
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json',
+     'Authorization': `Bearer ${getToken()}`,  
+   },
+   body: JSON.stringify(requestData),
+ })
+ .then(response => response.json())
+
+ .catch(error => {
+   console.error('Error:', error);
+   alert("Failed to create the notication.");
+ });
+ alert(`${game.itemName} has been requested.`);
+}
 
 
   return (
@@ -16,7 +54,7 @@ export default function GamesProfileCard({ game }) {
         <img src={game.img} />
         <div className="ItemCard-header">
           <h1>{game.itemName.length > MAX_CHAR ? game.itemName.substring(0, MAX_CHAR) + "..." : game.itemName}</h1>
-          <button className="borrow-button">Borrow</button>
+          <button className="borrow-button" onClick={() => borrowGame()}>Borrow</button>
         </div>
       </div>
       <div className="white-card-overlay">
